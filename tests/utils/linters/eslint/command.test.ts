644d1c0229cfb761f919520e3b5c4b5bcf6adf38
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-04-15 13:30:46 +0200

    chore: Standardized test names

--- tests/utils/linters/eslint/command.test.ts
+++ tests/utils/linters/eslint/command.test.ts
@@ -2,7 +2,7 @@ import { assert, test } from '@codemod-utils/tests';
 
 import { command } from '../../../../src/utils/linters/eslint.js';
 
-test('utils | lint-files | eslint > command', function () {
+test('utils | linters | eslint > command', function () {
   assert.strictEqual(
     command,
     './node_modules/.bin/eslint --format json --output-file .ignore-lint-errors/eslint.txt --quiet',
