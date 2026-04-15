e1dce3707839752aae96bbb2086a241db2f39a5d
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-04-15 13:39:47 +0200

    feature: Allowed codemod to ignore errors from typescript

--- src/steps/ignore-errors.ts
+++ src/steps/ignore-errors.ts
@@ -1,5 +1,8 @@
 import type { Options } from '../types/index.js';
-import { ignoreErrorsFromEslint } from './ignore-errors/index.js';
+import {
+  ignoreErrorsFromEslint,
+  ignoreErrorsFromTypescript,
+} from './ignore-errors/index.js';
 
 export function ignoreErrors(options: Options): void {
   const { dependencies, linter } = options;
@@ -8,6 +11,14 @@ export function ignoreErrors(options: Options): void {
     case 'eslint': {
       if (dependencies.eslint) {
         ignoreErrorsFromEslint(options);
+      }
+
+      break;
+    }
+
+    case 'typescript': {
+      if (dependencies.typescript) {
+        ignoreErrorsFromTypescript(options);
       }
 
       break;
--- src/utils/linters/typescript.ts
+++ src/utils/linters/typescript.ts
@@ -1,1 +1,73 @@
+import { EOL } from 'node:os';
+
 export const outputFilePath = '.ignore-lint-errors/typescript.txt';
+
+type LintError = {
+  line: number;
+  message: string;
+};
+
+type FilesWithErrors = {
+  filePath: string;
+  lintErrors: LintError[];
+};
+
+export function parseOutputFile(file: string): FilesWithErrors[] {
+  const filePathToData = new Map<string, Map<number, string[]>>();
+
+  file.split(EOL).forEach((str) => {
+    const matches = str.match(/^(.+)\((\d+),\d+\): error TS\d+: (.+)\.$/) as
+      | [ignore: unknown, filePath: string, line: string, message: string]
+      | null;
+
+    if (matches === null) {
+      return;
+    }
+
+    const filePath = matches[1];
+    const line = Number.parseInt(matches[2]);
+    const message = matches[3];
+
+    if (filePathToData.has(filePath)) {
+      if (filePathToData.get(filePath)!.has(line)) {
+        filePathToData.get(filePath)!.get(line)!.push(message);
+      } else {
+        filePathToData.get(filePath)!.set(line, [message]);
+      }
+    } else {
+      filePathToData.set(filePath, new Map([[line, [message]]]));
+    }
+  });
+
+  const filesWithErrors: FilesWithErrors[] = [];
+
+  filePathToData.forEach((data, filePath) => {
+    const lintErrors: LintError[] = [];
+
+    data.forEach((_allMessages, line) => {
+      lintErrors.push({
+        line,
+        message: 'Incorrect type',
+      });
+    });
+
+    lintErrors.sort((a, b) => {
+      if (a.line < b.line) {
+        return 1;
+      }
+
+      if (b.line < a.line) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    filesWithErrors.push({
+      filePath,
+      lintErrors,
+    });
+  });
+
+  return filesWithErrors;
+}
