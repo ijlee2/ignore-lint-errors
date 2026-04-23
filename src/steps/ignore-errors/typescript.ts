3d61f4ccb99118e15a07cbc25360010bc236e435
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-04-23 20:59:19 +0200

    feature: Allowed @glint-nocheck as fallback

--- src/steps/ignore-errors/typescript.ts
+++ src/steps/ignore-errors/typescript.ts
@@ -2,10 +2,14 @@ import { readFileSync, writeFileSync } from 'node:fs';
 import { EOL } from 'node:os';
 import { join } from 'node:path';
 
+import { updateTemplates } from '@codemod-utils/ast-template-tag';
 import { removeFiles } from '@codemod-utils/files';
 
 import type { Options } from '../../types/index.js';
-import { findLinesWithTemplate } from '../../utils/ignore-errors/typescript.js';
+import {
+  findLinesWithTemplate,
+  isParseable,
+} from '../../utils/ignore-errors/typescript.js';
 import {
   outputFilePath,
   parseOutputFile,
@@ -19,8 +23,9 @@ export function ignoreErrorsFromTypescript(options: Options): void {
 
   filesWithErrors.forEach(({ filePath, lintErrors }) => {
     const file = readFileSync(join(projectRoot, filePath), 'utf8');
-    const lines = file.split(EOL);
     const linesWithTemplate = findLinesWithTemplate(file);
+
+    let lines = file.split(EOL);
 
     lintErrors.forEach(({ line, message }) => {
       const erroredInTemplate = linesWithTemplate.some(
@@ -36,7 +41,45 @@ export function ignoreErrorsFromTypescript(options: Options): void {
       lines.splice(line - 1, 0, ignoreDirective);
     });
 
-    writeFileSync(join(projectRoot, filePath), lines.join(EOL), 'utf8');
+    let newFile = lines.join(EOL);
+
+    if (isParseable(newFile)) {
+      writeFileSync(join(projectRoot, filePath), newFile, 'utf8');
+      return;
+    }
+
+    // Ignore type checks in templates
+    lines = file.split(EOL);
+    let hasErrorInTemplate = false;
+
+    lintErrors.forEach(({ line, message }) => {
+      const erroredInTemplate = linesWithTemplate.some(
+        ([lineStart, lineEnd]) => {
+          return lineStart <= line && line <= lineEnd;
+        },
+      );
+
+      if (erroredInTemplate) {
+        hasErrorInTemplate = true;
+        return;
+      }
+
+      const ignoreDirective = `// @ts-expect-error: ${message}`;
+
+      lines.splice(line - 1, 0, ignoreDirective);
+    });
+
+    newFile = lines.join(EOL);
+
+    if (hasErrorInTemplate) {
+      newFile = updateTemplates(newFile, (code) => {
+        const ignoreDirective = '{{! @glint-nocheck }}';
+
+        return [ignoreDirective, code].join(EOL);
+      });
+    }
+
+    writeFileSync(join(projectRoot, filePath), newFile, 'utf8');
   });
 
   removeFiles([outputFilePath], { projectRoot });
